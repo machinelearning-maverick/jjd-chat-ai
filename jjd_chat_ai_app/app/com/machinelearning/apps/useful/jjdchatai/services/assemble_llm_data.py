18678320074ee210059c72813554a210a02a3d2a
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-01-12 20:02:40 +0000

    fix(app): vector store check if exists

--- jjd_chat_ai_app/app/com/machinelearning/apps/useful/jjdchatai/services/assemble_llm_data.py
+++ jjd_chat_ai_app/app/com/machinelearning/apps/useful/jjdchatai/services/assemble_llm_data.py
@@ -1,3 +1,5 @@
+import os.path
+
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -14,14 +16,19 @@ def chunk_data(data, chunk_size=256, chunk_overlap=20):
 
 def create_embeddings(chunks, persist_directory="./chroma_db"):
     print(f"create_embeddings()")
-    embeddings = OpenAIEmbeddings()
-    vector_store = Chroma.from_documents(chunks, embeddings, persist_directory=persist_directory)
+    if os.path.exists(persist_directory) and os.listdir(persist_directory):
+        print("Persist directory is not empty. Loading existing vector store.")
+        vector_store = Chroma(persist_directory=persist_directory)
+    else:
+        print("Persist directory is empty. Creating new vector store.")
+        embeddings = OpenAIEmbeddings()
+        vector_store = Chroma.from_documents(chunks, embeddings, persist_directory=persist_directory)
     print("Embeddings created")
     return vector_store
 
 def ask_and_get_answer(vector_store, q, k=3):
     print(f"ask_and_get_answer()")
-    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
     retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": k})
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     answer = chain.invoke(q)
