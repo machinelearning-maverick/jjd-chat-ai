a3f6ffeab9ec8e43e68578bc37dad70cf1f51549
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-01-21 20:40:21 +0000

    feat(core): add one-time initialization

--- jjd_chat_ai_web/web/com/machinelearning/apps/useful/jjdchatai/web/web_app.py
+++ jjd_chat_ai_web/web/com/machinelearning/apps/useful/jjdchatai/web/web_app.py
@@ -25,36 +25,45 @@ def session_management():
         logging.info("Session state created")
 
 
-def main():
-    logging.info("BEGIN: main()")
+class JJDChatAIWebApp:
+    _initialized = False
 
-    st.title("Question and Answer Chat")
-    st.write("Question and Answer Chat for the Blog content - powered by AI")
+    def __init__(self):
+        logger.info("Web App initialized for this session.")
+        self.cc_chain = None
 
-    with st.form("qanda_form"):
-        question = st.text_input("Write question:")
-        clicked = st.form_submit_button("Ask question")
-
-    try:
+    @st.cache_resource
+    def startup_task(_self):
+        logger.info("startup_task()")
         session_management()
+        cc_chain = cc.create_conversational_chat(st.session_state.vs)
+        return {"initialized": True, "cc_chain": cc_chain}
 
-        if clicked:
-            # llm_answer = ald.ask_and_get_answer(st.session_state.vs, question)
-            # llm_answer = cc.create_conversational_chat(st.session_state.vs, question)
 
-            unset_chain = object()
-            chain = unset_chain
-            if chain is unset_chain:
-                chain = cc.create_conversational_chat(st.session_state.vs)
-            llm_answer = cc.ask_question(question, chain)
+    def run(self):
+        logging.info("BEGIN: run()")
 
-            answer = st.text_area("Answer:", value=llm_answer)
-    except Exception as e:
-        logging.error("Error during calculation", exc_info=True)
-        st.error("An unexpected error occurred. Please try again.")
+        startup_status = self.startup_task()
+        self.cc_chain = startup_status["cc_chain"]
 
-    logging.info("END: main()")
+        st.title("Question and Answer Chat")
+        st.write("Question and Answer Chat for the Blog content - powered by AI")
+
+        with st.form("qanda_form"):
+            question = st.text_input("Write question:")
+            clicked = st.form_submit_button("Ask question")
+
+        try:
+            if clicked:
+                llm_answer = cc.ask_question(question, self.cc_chain)
+                answer = st.text_area("Answer:", value=llm_answer)
+        except Exception as e:
+            logging.error("Error during calculation", exc_info=True)
+            st.error("An unexpected error occurred. Please try again.")
+
+        logging.info("END: run()")
 
 
 if __name__ == "__main__":
-    main()
+    web_app = JJDChatAIWebApp()
+    web_app.run()
