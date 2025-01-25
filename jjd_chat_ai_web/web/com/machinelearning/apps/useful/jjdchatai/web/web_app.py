decda70bf2238c2ca0aa575772a43987a91a64ed
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-01-25 15:00:08 +0000

    refactor(core): replace streamlit_chat with simple Markdown; logger configuration

--- jjd_chat_ai_web/web/com/machinelearning/apps/useful/jjdchatai/web/web_app.py
+++ jjd_chat_ai_web/web/com/machinelearning/apps/useful/jjdchatai/web/web_app.py
@@ -1,12 +1,11 @@
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv(), override=True)
 
 import yaml
 import logging
 import logging.config
 import streamlit as st
-from streamlit_chat import message
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
 
 import app.com.machinelearning.apps.useful.jjdchatai.services.vector_store_service as vss
 import app.com.machinelearning.apps.useful.jjdchatai.services.conversational_chat as cc
@@ -33,14 +32,6 @@ class JJDChatAIWebApp:
         logger.info("Web App initialized for this session.")
         self.cc_chain = None
 
-    @st.cache_resource
-    def startup_task(_self):
-        logger.info("startup_task()")
-        session_management()
-        cc_chain = cc.create_conversational_chat(st.session_state.vs)
-        return {"initialized": True, "cc_chain": cc_chain}
-
-
     def run(self):
         logging.info("BEGIN: run()")
 
@@ -54,22 +45,33 @@ class JJDChatAIWebApp:
             question = st.text_input("Write question:")
             clicked = st.form_submit_button("Ask question")
 
-        try:
-            if clicked:
-                llm_answer = cc.ask_question(question, self.cc_chain)
-                # answer = st.text_area("Answer:", value=llm_answer)
+            try:
+                if clicked:
+                    if question.strip():
+                        llm_answer = cc.ask_question(question, self.cc_chain)
+                        if llm_answer:
+                            chat_history = llm_answer["chat_history"]
+                            answer_pairs = [(chat_history[i], chat_history[i + 1]) for i in
+                                            range(0, len(chat_history) - 1, 2)]
 
-                if llm_answer:
-                    for history in llm_answer["chat_history"]:
-                        if isinstance(history, HumanMessage):
-                            message(history.content, is_user=True)
-                        elif isinstance(history, AIMessage):
-                            message(history.content, is_user=False)
-        except Exception as e:
-            logging.error("Error during calculation", exc_info=True)
-            st.error("An unexpected error occurred. Please try again.")
+                            for answer in reversed(answer_pairs):
+                                st.divider()
+                                st.markdown(f":robot_face: **AI:** {answer[0].content}")
+                                st.markdown(f":man: **You:** {answer[1].content}")
+                    else:
+                        st.error("Please enter a question.")
+            except Exception as e:
+                logging.error("Error during calculation", exc_info=True)
+                st.error("An unexpected error occurred. Please try again.")
 
         logging.info("END: run()")
+
+    @st.cache_resource
+    def startup_task(_self):
+        logger.info("startup_task()")
+        session_management()
+        cc_chain = cc.create_conversational_chat(st.session_state.vs)
+        return {"initialized": True, "cc_chain": cc_chain}
 
 
 if __name__ == "__main__":
